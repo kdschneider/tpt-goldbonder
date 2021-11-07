bf14917a678251a0f0d227f2666542f37c8b95ad
Konstantin Schneider <ks@konstantin-schneider.eu>
2021-11-07 19:56:11 +0100

    Update _build.R

--- _build.R
+++ _build.R
@@ -1,5 +1,5 @@
 # build book
 source(here::here("R/purl_rmd.R"))
 source(here::here("R/run_scripts.R"))
-source(here::here("R/render_book.R"))
+source(here::here("R/render_bookdown.R"))
 source(here::here("R/render_rmd.R"))
