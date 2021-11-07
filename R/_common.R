a1c34b1734bca90efd5027b76e9c91b3424c701b
Konstantin Schneider <ks@konstantin-schneider.eu>
2021-11-07 04:41:57 +0100

    Update R/

--- R/_common.R
+++ R/_common.R
@@ -1,8 +1,9 @@
 # example R options set globally
-options(width = 60)
+options(width = 80)
 
 # example chunk options set globally
 knitr::opts_chunk$set(
   comment = "#>",
-  collapse = TRUE
-  )
+  collapse = TRUE,
+  fig.align = "center"
+)
---
+++ R/custom_functions.R
@@ -0,0 +1,59 @@
+# custom functions
+
+# check if package is loaded
+check_loaded <- function(package) {
+  package %in% .packages()
+}
+
+# rescale factors
+rescale_factors <- function(data, factors) {
+
+  res <-
+    data |>
+    mutate(
+      across(
+        .cols = {{ factors }},
+        .fns = function(x) {
+          scales::rescale(x, to = c(-1,1))
+        }
+      )
+    )
+
+  res
+
+}
+
+create_effect_plot <- function(data, factors, response) {
+
+  dat <-
+    data |>
+    rescale_factors({{ factors }}) |>
+    pivot_longer(cols = {{ factors }}) |>
+    group_by(name, value) |>
+    summarise({{ response }} := mean({{ response }})) |>
+    drop_na()
+
+  plot <-
+    dat |>
+    ggplot(
+      aes(
+        x = value,
+        y = {{ response }}
+      )
+    ) +
+    geom_line() +
+    geom_point() +
+    facet_wrap(vars(name), ncol = 3)
+
+  return(plot)
+
+}
+
+### plotly/ggplot for html/pdf
+show_plot <- function(plot) {
+  if (knitr::is_html_output() == TRUE) {
+    return(plotly::ggplotly(plot))
+  } else {
+    return(plot)
+  }
+}
