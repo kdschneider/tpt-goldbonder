a9a9b5cc912638e29aa1d3467a34515486288543
Konstantin Schneider <ks@konstantin-schneider.eu>
2021-11-07 07:28:25 +0100

    Create render_book.R

---
+++ R/render_book.R
@@ -0,0 +1,15 @@
+# render book
+
+## render bs4_book
+bookdown::render_book(
+  input = here::here(""),
+  output_format = "bookdown::bs4_book",
+  output_dir = here::here("_book")
+)
+
+## render pdf
+bookdown::render_book(
+  input = here::here(""),
+  output_format = "bookdown::pdf_document2",
+  output_dir = here::here("_book/downloads")
+)
