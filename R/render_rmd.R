688bfdfded1aa6472b251cab0b7cb98f14b21549
Konstantin Schneider <ks@konstantin-schneider.eu>
2021-11-07 18:01:27 +0100

    Automate rmd rendering

--- R/render_rmd.R
+++ R/render_rmd.R
@@ -1,2 +1,24 @@
 # render rmds
 
+fs::dir_create(here::here("_book/downloads/analysis/"))
+
+
+## get file paths
+files_path <- fs::dir_ls(
+  path = here::here("Rmd"),
+  glob = "*.Rmd"
+)
+
+## render
+
+custom_render <- function(input) {
+  rmarkdown::render(
+    input = input,
+    output_dir = here::here("_book/downloads/analysis/")
+  )
+}
+
+purrr::walk(
+  .x = files_path,
+  .f = custom_render
+)
--- _build.R
+++ _build.R
@@ -2,3 +2,4 @@
 source(here::here("R/purl_rmd.R"))
 source(here::here("R/run_scripts.R"))
 source(here::here("R/render_book.R"))
+source(here::here("R/render_rmd.R"))
